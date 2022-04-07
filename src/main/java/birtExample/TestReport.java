ca70b14e865856a64bef8ce6aef6ec3f528a097a
Krishna Prasad <krishna.p@tbitsglobal.com>
2022-04-07 09:00:14 +0530

    Reading report directly from the project hierarchy

--- src/main/java/birtExample/TestReport.java
+++ src/main/java/birtExample/TestReport.java
@@ -1,5 +1,8 @@
 package birtExample;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.core.framework.Platform;
 import org.eclipse.birt.report.engine.api.EngineConfig;
@@ -14,14 +17,15 @@ import org.eclipse.birt.report.model.api.SlotHandle;
 import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
 public class TestReport {
 
-	public static void main(String[] args) throws BirtException {
+	public static void main(String[] args) throws BirtException, IOException {
 		// TODO Auto-generated method stub
 		EngineConfig config = new EngineConfig();
 		Platform.startup();
 		IReportEngineFactory factory = (IReportEngineFactory) Platform.
 				createFactoryObject(IReportEngineFactory.EXTENSION_REPORT_ENGINE_FACTORY);
 		IReportEngine engine = factory.createReportEngine(config);
-		IReportRunnable design = engine.openReportDesign("D:\\landscape.rptdesign");
+		URL url = TestReport.class.getClassLoader().getResource("landscape.rptdesign");
+		IReportRunnable design = engine.openReportDesign(url.openStream());
 		SlotHandle sd = design.getDesignHandle().getModuleHandle().getMasterPages();
 		MasterPageHandle h =  design.getDesignHandle().getModuleHandle().findMasterPage(sd.get(0).getFullName());
 		h.setOrientation(DesignChoiceConstants.PAGE_ORIENTATION_PORTRAIT);
@@ -31,7 +35,6 @@ public class TestReport {
 		String outPutFileName = "D:\\"+ h.getOrientation() + "." + IRenderOption.OUTPUT_FORMAT_PDF;
 		renderOptions.setOutputFileName(outPutFileName);
 		task.setRenderOption(renderOptions);
-		
 		task.run();
 		task.close();
 	}
